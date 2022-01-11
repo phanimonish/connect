0c63aa8ce6ea1f9921cb05bfb7640809525e649a
phanimonish <67011446+phanimonish@users.noreply.github.com>
2022-01-11 13:25:54 +0530

    this is my first commit

---
+++ lib/services/auth.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:connect/helperfunctions/sharedpref_helper.dart';
+import 'package:connect/services/database.dart';
+import 'package:connect/views/home.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class AuthMethods {
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  getCurrentUser() async {
+    return await auth.currentUser;
+  }
+
+  signInWithGoogle(BuildContext context) async {
+    final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+
+    final GoogleSignInAccount googleSignInAccount =
+        await _googleSignIn.signIn();
+
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken);
+
+    UserCredential result =
+        await _firebaseAuth.signInWithCredential(credential);
+
+    User userDetails = result.user;
+
+    if (result != null) {
+      SharedPreferenceHelper().saveUserEmail(userDetails.email);
+      SharedPreferenceHelper().saveUserId(userDetails.uid);
+      SharedPreferenceHelper().saveDisplayName(userDetails.displayName);
+      SharedPreferenceHelper().saveUserProfileUrl(userDetails.photoURL);
+    }
+  }
+}
---
+++ lib/views/signin.dart
@@ -0,0 +1,19 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class SignIN extends StatefulWidget {
+  @override
+  _SignINState createState() => _SignINState();
+}
+
+class _SignINState extends State<SignIN> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Connect"),
+      ),
+    );
+  }
+}
